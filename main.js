b68e1958c63dd646ec96275994e4fd07c498cd90
farizwebdev <farizabar80@gmail.com>
2025-12-29 21:17:32 +0700

    ubah

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ for (let item of kotak) {
 // Variabel untuk menampung elemen dengan kelas 'Kotak1'
 const kotak1 = document.querySelector('.Kotak1');
 console.log(kotak1);
+
+// Membuat Elemen baru Pakai DOM
+const paragrafBaru = document.createElement('p');
+paragrafBaru.innerText = 'Paragraf Baru dari DOM';
+
+// Menambahkan gambar Pakai DOM
+const gambarBaru = document.createElement('img');
+gambarBaru.setAttribute('src', 'https://placeimg.com/200/200/animals');
+gambarBaru.setAttribute('alt', 'Gambar Hewan');
