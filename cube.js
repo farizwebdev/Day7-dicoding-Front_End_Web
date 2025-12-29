2a862512c79838d856a20e4f3d839ccb125c5712
farizwebdev <farizabar80@gmail.com>
2025-12-29 21:43:26 +0700

    ubah

--- cube.js
+++ cube.js
@@ -2,9 +2,11 @@
 const gambar = document.getElementById('gambar');
 console.log(gambar);
 
+// Mengubah atribut width dan height pada elemen gambar
 gambar.setAttribute('width', 300);
 gambar.setAttribute('height', 200);
 
+// Menonaktifkan tombol Play pada elemen dengan kelas 'button'
 const button = document.querySelectorAll('.button');
 const buttonPlay = button[3];
 const buttonPlayElement = buttonPlay.children[0];
