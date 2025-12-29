04496375a00a1e4163f07eaa8cb7de61ac35b44d
farizwebdev <farizabar80@gmail.com>
2025-12-29 21:14:35 +0700

    ubah

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+// Variabel untuk menampung semua elemen dengan kelas 'Kotak'
 const kotak = document.querySelectorAll('.Kotak');
 
+
+// Menampilkan setiap elemen yang ditemukan di konsol
 for (let item of kotak) {
     console.log(item);
 }
 
+// Variabel untuk menampung elemen dengan kelas 'Kotak1'
 const kotak1 = document.querySelector('.Kotak1');
 console.log(kotak1);
