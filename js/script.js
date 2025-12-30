552d227bb91a85828ff6ae4a9336f1789f02325b
farizwebdev <farizabar80@gmail.com>
2025-12-30 11:47:36 +0700

    ubah

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
         addTodo();
     });
 });
+
+function addTodo() {
+  const textTodo = document.getElementById('title').value;
+  const timestamp = document.getElementById('date').value;
+ 
+  const generatedID = generateId();
+  const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+  todos.push(todoObject);
+ 
+  document.dispatchEvent(new Event(RENDER_EVENT));
+};
+
