f5bdfa83b25cfbfe7be969798b755ddc5b3e4d66
farizwebdev <farizabar80@gmail.com>
2025-12-30 12:52:06 +0700

    ubah

--- js/script.js
+++ js/script.js
@@ -39,3 +39,21 @@ document.addEventListener(RENDER_EVENT, function () {
 });
 
 // Membuat Logika Menampilkan Todo
+function makeTodo(todoObject) {
+  const textTitle = document.createElement('h2');
+  textTitle.innerText = todoObject.task;
+ 
+  const textTimestamp = document.createElement('p');
+  textTimestamp.innerText = todoObject.timestamp;
+ 
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('inner');
+  textContainer.append(textTitle, textTimestamp);
+ 
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow');
+  container.append(textContainer);
+  container.setAttribute('id', `todo-${todoObject.id}`);
+ 
+  return container;
+}
