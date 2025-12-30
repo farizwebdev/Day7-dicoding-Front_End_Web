2c4c0bfaf6df2fb46bc43b7641233e5c7becacd2
farizwebdev <farizabar80@gmail.com>
2025-12-30 07:54:06 +0700

    ubah

--- Interaktif Event/event.js
+++ Interaktif Event/event.js
@@ -1,7 +1,9 @@
+// Membuat sebuah Fungsi welcome
 function welcome() {
     alert("Sim salabim abis ini terjadi apa?");
     const contents = document.querySelector(".contents");
     contents.removeAttribute("hidden");
 }
 
+// Menjalankan Fungsi welcome ketika halaman selesain di load
 document.body.onload = welcome;
